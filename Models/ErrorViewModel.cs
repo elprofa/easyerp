08198bf69fd7db17dbebda0ec7216dc04a8dc4cd
elprofa <wedoprofan14@gmail.com>
2021-07-27 09:26:07 +0100

    yaya first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exemple_pratique.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
